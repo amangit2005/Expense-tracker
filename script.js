f5e3f45ca1bcea83a46c29ebf03c6e45ef8a102f
amangit2005 <amanjain9065@gmail.com>
2026-01-20 19:39:33 +0530

    Add core expense logic (add, delete, balance)

---
+++ script.js
@@ -0,0 +1,87 @@
+// GET HTML ELEMENTS
+
+const form = document.querySelector(".add-expense form");
+const textInput = form.querySelector("input[type='text']");
+const amountInput = form.querySelector("input[type='number']");
+const balanceAmount = document.querySelector(".balance-amount");
+const expenseList = document.querySelector(".expense-list");
+
+// DATA STORAGE (IN MEMORY or local )
+
+let expenses = [];
+
+// HANDLE FORM SUBMIT 
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  const text = textInput.value.trim();
+  const amount = Number(amountInput.value);
+
+  // basic validation
+  if (text === "" || amount === 0) {
+    alert("Please enter valid expense details");
+    return;
+  }
+
+  const expense = {
+    id: Date.now(),
+    text: text,
+    amount: amount
+  };
+
+  expenses.push(expense);
+
+  addExpenseToDOM(expense);
+  updateBalance();
+
+  textInput.value = "";
+  amountInput.value = "";
+});
+
+// ADD EXPENSE TO UI
+
+function addExpenseToDOM(expense) {
+  const li = document.createElement("li");
+
+  li.innerHTML = `
+    <span>${expense.text}</span>
+    <span>₹${expense.amount}</span>
+    <button data-id="${expense.id}">❌</button>
+  `;
+
+  li.style.display = "flex";
+  li.style.justifyContent = "space-between";
+  li.style.alignItems = "center";
+  li.style.marginTop = "10px";
+
+  const deleteBtn = li.querySelector("button");
+  deleteBtn.addEventListener("click", function () {
+    removeExpense(expense.id);
+  });
+
+  expenseList.appendChild(li);
+}
+
+// REMOVE EXPENSE
+
+function removeExpense(id) {
+  expenses = expenses.filter(function (exp) {
+    return exp.id !== id;
+  });
+
+  expenseList.innerHTML = "";
+  expenses.forEach(addExpenseToDOM);
+
+  updateBalance();
+}
+
+// UPDATE BALANCE
+
+function updateBalance() {
+  const total = expenses.reduce(function (sum, exp) {
+    return sum + exp.amount;
+  }, 0);
+
+  balanceAmount.textContent = `₹${total}`;
+}
